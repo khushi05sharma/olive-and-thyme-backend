4d48b2a1dd7bb03cb2712bc35a551b580e044f84
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-06-13 19:45:18 +0530

    fixing so msgs

--- src/middleware/protect.ts
+++ src/middleware/protect.ts
@@ -3,8 +3,6 @@ import jwt from "jsonwebtoken";
 import User from "../models/User";
 
 // ------ EXTEND EXPRESS REQUEST TYPE -------
-// by default req has: body, headers, params, query etc
-// we need to ADD a user field so we can attach the logged-in user to it
 // this tells TypeScript: req.user is allowed and has this shape
 
 declare global {
@@ -25,7 +23,7 @@ const protect = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer")) {
-      console.log(`[PROTECT] Blocked >> no token on ${req.method} ${req.url}`);
+      console.log("[PROTECT] Blocked: no token in this req");
       return res
         .status(401)
         .json({ message: "Not authorized - please login first" });
@@ -35,7 +33,6 @@ const protect = async (req: Request, res: Response, next: NextFunction) => {
     const token = authHeader.split(" ")[1];
 
     // verify token
-    // if expired or tampered -> jwt.verify throws error -> caught below
     if (!process.env.JWT_SECRET) {
       throw new Error("JWT_SECRET is not defined in .env file");
     }
@@ -53,8 +50,6 @@ const protect = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // attach user to request object
-    // now any route using protect can access req.user
-    // e.g. req.user.id to know who is saving/liking/commenting
     req.user = {
       id: user._id.toString(),
       name: user.name,
